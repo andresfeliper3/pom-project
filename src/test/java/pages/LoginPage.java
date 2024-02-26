e09530dc26be46fe817ee2d81191af70d3d645fa
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-02-25 23:26:18 -0500

    BasePage, LoginPage, BaseTest and PurchaseProductTest first version created

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+    @FindBy(name = "login-button")
+    private WebElement loginButton;
+
+
+    public LoginPage(WebDriver webDriver, String url) {
+        super(webDriver);
+    }
+
+    public void login(String username, String password) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+}
